a98e2e923e771adc8e5eff02b1d94e95d8625f56
Rich Budek <RichBu@hotmail.com>
2020-05-30 14:53:53 -0500

    Initial app developed
    tabbed menus added now adding content

---
+++ PinDecoder/ViewModels/InfoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+
+namespace PinDecoder.ViewModels
+{
+    public class InfoViewModel : BaseViewModel
+    {
+        public InfoViewModel()
+        {
+            Title = "Info";
+        }
+    }
+}
