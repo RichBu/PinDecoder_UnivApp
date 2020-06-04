121c577ec599a72532c553353f8627160f8e927f
Rich Budek <RichBu@hotmail.com>
2020-06-03 23:56:38 -0500

    * PinDecode_Banner_01d.png: New Banner picture
    * PicturePage.xaml.cs:
    * PinDecode_Icon_01.png:
    
    * PinDecode_Pic_01.png: Explanation picture
    
    * PictureViewModel.cs: Picture to pop up
    
    * PicturePage.xaml: New Picture PAge

---
+++ PinDecoder/ViewModels/PictureViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+using System.Collections.Generic;
+
+
+
+namespace PinDecoder.ViewModels
+{
+    public class PictureViewModel : BaseViewModel
+    {
+        public PictureViewModel()
+        {
+            Title = "Picture Explanation";
+            PictPageCommand = new Command(async () => await Application.Current.MainPage.Navigation.PushModalAsync(new NavigationPage(new Views.PicturePage())));
+        }
+
+
+        public async void PictButton_Clicked()
+        {
+            //Application.Current.MainPage.Navigation
+            Console.WriteLine("***** RPB ******");
+            Console.WriteLine("pict button clicked");
+            await Application.Current.MainPage.Navigation.PushModalAsync(new NavigationPage(new Views.PicturePage()));
+        }
+
+        public ICommand PictPageCommand { get; }
+        /*
+        public void PictButton_Clicked()
+        {
+            //clicked the picture button
+        }
+        */
+    }
+}
---
+++ PinDecoder/Views/PicturePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PinDecoder.Views
+{
+    public partial class PicturePage : ContentPage
+    {
+        public PicturePage()
+        {
+            InitializeComponent();
+        }
+
+        async void Back_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
